b4a70ca376bdef48f3595d235e9693c73fb5c8b1
fschaer <frederic.schaer@cea.fr>
2013-12-04 15:20:33 +0100

    allow for paths customization in ERB, allow user to override template

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,12 +4,20 @@ class apelparser::config (
   $mysql_database = 'apelclient',
   $mysql_user     = 'apel',
   $mysql_password = 'changeme',
-  $site_name      = 'UKI-SOUTHGRID-XXX-HEP',) {
+  $site_name      = 'GOCDB-NAME',
+  
+  #this may vary, depending on the packages
+  $torque_dir='/var/torque/server_priv/accounting', 
+  $blah_dir='/var/log/cream/accounting',
+  
+  #don't change, unless you know what you're doing
+  $template_name="${module_name}/parser.cfg.erb"
+  ) {
   file { '/etc/apel/parser.cfg':
     owner   => "root",
     group   => "root",
     ensure  => "present",
-    content => template("${module_name}/parser.cfg.erb"),
+    content => template($template_name),
     require => Package['apel-parsers'],
   }
 }
